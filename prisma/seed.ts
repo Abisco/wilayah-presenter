bde6de03cbdb9749bde07b61595078ac6e838df4
Abbass-Ali Siwjee <abbassali@usemotion.com>
2023-02-14 22:29:33 -0500

    Adjust seeds, remove unused story

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,165 +1,191 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import * as PrismaClient from '@prisma/client'
-import fs from 'fs'
-const prisma = new PrismaClient.PrismaClient()
+import * as PrismaClient from "@prisma/client";
+import type { Language } from "@prisma/client";
+import fs from "fs";
+const prisma = new PrismaClient.PrismaClient();
 
 interface QuranListEntry {
-  id: number
-  name: string //arabic
-  transliteration: string,
-  translation: string,
-  type: string,
-  total_verses: number,
-  link: string,
+  id: number;
+  name: string; //arabic
+  transliteration: string;
+  translation: string;
+  type: string;
+  total_verses: number;
+  link: string;
 }
 
 interface QuranVerseEntry {
-  id: number
-  surah: number
-  ayah: number
-  verse: string
+  id: number;
+  surah: number;
+  ayah: number;
+  verse: string;
 }
 
-interface QuranVersesObject { 
+interface QuranVersesObject {
   [key: string]: {
     [key: string]: {
-      [id: number]: QuranVerseEntry
-    }
-  }
- }
+      [id: number]: QuranVerseEntry;
+    };
+  };
+}
 
-const rawQuranList = fs.readFileSync('./prisma/quranFiles/quranList.json', "utf8");
+const rawQuranList = fs.readFileSync(
+  "./prisma/quranFiles/quranList.json",
+  "utf8"
+);
 const quranList = JSON.parse(rawQuranList) as QuranListEntry[];
 
-const rawUthmaniQuran = fs.readFileSync('./prisma/quranFiles/quran-uthmani.json', "utf8");
+const rawUthmaniQuran = fs.readFileSync(
+  "./prisma/quranFiles/quran-uthmani.json",
+  "utf8"
+);
 const uthmaniQuran = JSON.parse(rawUthmaniQuran) as QuranVersesObject;
 
-const rawShakirQuran = fs.readFileSync('./prisma/quranFiles/en-shakir.json', "utf8");
+const rawShakirQuran = fs.readFileSync(
+  "./prisma/quranFiles/en-shakir.json",
+  "utf8"
+);
 const shakirQuran = JSON.parse(rawShakirQuran) as QuranVersesObject;
 
+const rawYusufAliQuran = fs.readFileSync(
+  "./prisma/quranFiles/en-yusufali.json",
+  "utf8"
+);
+const yusufaliQuran = JSON.parse(rawYusufAliQuran) as QuranVersesObject;
+
+const rawSahihQuran = fs.readFileSync(
+  "./prisma/quranFiles/en-sahih.json",
+  "utf8"
+);
+const sahihQuran = JSON.parse(rawSahihQuran) as QuranVersesObject;
+
+const languageBismillah: {
+  [key in Language]: string;
+} = {
+  ARABIC: "بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ",
+  ENGLISH: "In the name of Allah, the Beneficent, the Merciful.",
+  URDU: "",
+  FRENCH: "",
+};
+
+const createSource = async (
+  sourceObject: QuranVersesObject,
+  language: Language,
+  source: string,
+  sourceName: string
+) => {
+  console.log("Creating source: ", sourceName);
+  const versesData: PrismaClient.Prisma.Enumerable<PrismaClient.Prisma.VerseCreateManyInput> =
+    [];
+  let bismillahsAdded = 0;
+
+  // Check if the source exists
+  const sourceExists = await prisma.verse.findFirst({
+    where: {
+      source: sourceName,
+      surahNumber: 1,
+    },
+  });
+
+  if (!sourceExists) {
+    for (const verseNum in sourceObject?.quran?.[source] ?? {}) {
+      const verse = sourceObject?.quran?.[source]?.[verseNum];
+
+      if (verse) {
+        const {
+          id: overallVerseNumber,
+          surah: surahNumber,
+          ayah: verseNumber,
+          verse: text,
+        } = verse;
+
+        if (surahNumber !== 1 && surahNumber !== 9 && verseNumber === 1) {
+          versesData.push({
+            overallVerseNumber: overallVerseNumber + bismillahsAdded,
+            surahNumber,
+            verseNumber: 0,
+            text: languageBismillah[language],
+            source: sourceName,
+            language,
+          });
+
+          bismillahsAdded++;
+        }
+
+        versesData.push({
+          overallVerseNumber: overallVerseNumber + bismillahsAdded,
+          surahNumber,
+          verseNumber,
+          text,
+          source: sourceName,
+          language,
+        });
+      }
+    }
+
+    await prisma.verse.createMany({
+      data: versesData,
+    });
+
+    console.log("Source created: ", sourceName);
+    return "Source created!";
+  } else {
+    console.log("Source already exists: ", sourceName);
+    return "Source already exists!";
+  }
+};
 
 async function main() {
   // Remove all previous quran index
-  await prisma.quranIndex.deleteMany({
-    where: {
-      id: {
-        gte: 0
-      }
+  await prisma.$transaction(async (tx) => {
+    const firstEntry = await tx.quranIndex.findFirst({
+      where: {
+        surahNameArabic: quranList[0]?.name,
+        surahNameEnglish: quranList[0]?.translation,
+        surahNameTransliteration: quranList[0]?.transliteration,
+      },
+    });
+
+    if (!firstEntry) {
+      // Insert many verses
+      await tx.quranIndex.createMany({
+        data: quranList.map((quranEntry) => {
+          return {
+            surahNumber: quranEntry.id,
+            surahNameArabic: quranEntry.name,
+            surahNameTransliteration: quranEntry.transliteration,
+            surahNameEnglish: quranEntry.translation,
+            type: quranEntry.type === "meccan" ? "MAKKAH" : "MADINAH",
+            versesCount: quranEntry.total_verses,
+          };
+        }),
+      });
+
+      console.log("Quran index created!");
+      return "Success";
     }
-  })
-
-  // Insert many verses
-  await prisma.quranIndex.createMany({
-    data: quranList.map((quranEntry) => {
-      return {
-        surahNumber: quranEntry.id,
-        surahNameArabic: quranEntry.name,
-        surahNameTransliteration: quranEntry.transliteration,
-        surahNameEnglish: quranEntry.translation,
-        type: quranEntry.type === 'meccan' ? "MAKKAH" : "MADINAH",
-        versesCount: quranEntry.total_verses,
-      }
-    })
-  })
-
-  // Remove all previous verses
-  await prisma.verse.deleteMany({
-    where: {
-      id: {
-        gte: 0
-      }
+    {
+      console.log("Quran index already exists!");
+      return "Already exists";
     }
-  })
+  });
 
-  const uthmaniVersesData: PrismaClient.Prisma.Enumerable<PrismaClient.Prisma.VerseCreateManyInput> = []
-  let uthmaniBismillahsAdded = 0
+  await createSource(uthmaniQuran, "ARABIC", "quran-uthmani", "Uthmani");
+  await createSource(shakirQuran, "ENGLISH", "en.shakir", "Shakir");
+  await createSource(yusufaliQuran, "ENGLISH", "en.yusufali", "Yusuf Ali");
+  await createSource(sahihQuran, "ENGLISH", "en.sahih", "Sahih International");
 
-  const shakirVersesData: PrismaClient.Prisma.Enumerable<PrismaClient.Prisma.VerseCreateManyInput>  = []
-  let shakirBismillahsAdded = 0
-
-  for (const uthmaniVerseNum in uthmaniQuran?.quran?.['quran-uthmani'] ?? {}) {
-    const verse = uthmaniQuran?.quran?.['quran-uthmani']?.[uthmaniVerseNum]
-
-    if (verse) {
-      const { id: overallVerseNumber, surah: surahNumber, ayah: verseNumber, verse: text } = verse
-      const language = 'ARABIC'
-      const source = 'Uthmani'
-
-      if (surahNumber !== 1 && surahNumber !== 9 && verseNumber === 1) {
-        uthmaniVersesData.push({
-          overallVerseNumber: overallVerseNumber + uthmaniBismillahsAdded,
-          surahNumber,
-          verseNumber: 0,
-          text: 'بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ',
-          source,
-          language
-        })
-
-        uthmaniBismillahsAdded++
-      }
-
-  
-      uthmaniVersesData.push({
-        overallVerseNumber: overallVerseNumber + uthmaniBismillahsAdded,
-        surahNumber,
-        verseNumber,
-        text,
-        source,
-        language
-      })
-    }
-  }
-
-  for (const shakirVerseNum in shakirQuran?.quran?.['en.shakir'] ?? {}) {
-    const verse = shakirQuran?.quran?.['en.shakir']?.[shakirVerseNum]
-
-    if (verse) {
-      const { id: overallVerseNumber, surah: surahNumber, ayah: verseNumber, verse: text } = verse
-      const language = 'ENGLISH'
-      const source = 'Shakir'
-
-      if (surahNumber !== 1 && surahNumber !== 9 && verseNumber === 1) {
-        shakirVersesData.push({
-          overallVerseNumber: overallVerseNumber + shakirBismillahsAdded,
-          surahNumber,
-          verseNumber: 0,
-          text: 'In the name of Allah, the Beneficent, the Merciful.',
-          source,
-          language
-        })
-
-        shakirBismillahsAdded++
-      }
-  
-      shakirVersesData.push({
-        overallVerseNumber: overallVerseNumber + shakirBismillahsAdded,
-        surahNumber,
-        verseNumber,
-        text,
-        source,
-        language
-      })
-    }
-  }
-
-  const uthmaniVerses = await prisma.verse.createMany({
-    data: uthmaniVersesData
-  })
-
-  const shakirVerses = await prisma.verse.createMany({
-    data: shakirVersesData
-  })
-
-  return { uthmaniVerses, shakirVerses }
+  return "Success!";
 }
+
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
